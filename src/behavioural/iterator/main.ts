e014b20724dace48883d05bb76a6db3ddfd71e6d
abreuitalo <italo.ab.leite@gmail.com>
2024-12-29 18:15:02 -0300

    iterator

---
+++ src/behavioural/iterator/main.ts
@@ -0,0 +1,19 @@
+import { MyDataStructure } from './my-data-structure';
+import { MyReverseIterator } from './my-reverse-iterator';
+
+const dataStructure = new MyDataStructure();
+dataStructure.addItem('A', 'B', 'C', 'D', 'E', 'F');
+
+const [a, b] = dataStructure;
+
+console.log(a, b);
+
+for (const data of dataStructure) {
+  console.log(data);
+}
+console.log();
+
+dataStructure.changeIterator(new MyReverseIterator(dataStructure));
+for (const data of dataStructure) {
+  console.log(data);
+}
